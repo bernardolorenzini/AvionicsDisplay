3b3638345a75782862cc42d25cdf14e2ba17290a
Bernardo Lorenzini <87033537+bernardolorenzini@users.noreply.github.com>
2023-08-09 23:12:16 -0300

    Update symbologyUploading.hpp

--- HUD/OpenCV_Course02/symbologyUploading.hpp
+++ HUD/OpenCV_Course02/symbologyUploading.hpp
@@ -13,7 +13,15 @@ cv::Mat uaRing = cv::imread("/Users/bernardolorenzini/Documents/MyGitRepositorie
 cv::Mat pitchArrow = cv::imread("/Users/bernardolorenzini/Documents/MyGitRepositories/Textures/Pitch_Arrow.png", cv::IMREAD_UNCHANGED);
 cv::Mat fpvGround = cv::imread("/Users/bernardolorenzini/Documents/MyGitRepositories/Textures/FPV_Ground.png", cv::IMREAD_UNCHANGED);
 cv::Mat fpvDashed = cv::imread("/Users/bernardolorenzini/Documents/MyGitRepositories/Textures/FPV_Dashed.png", cv::IMREAD_UNCHANGED);
+cv::Mat SpeedScaleIndicator = cv::imread("/Users/bernardolorenzini/Documents/MyGitRepositories/Textures/Speed_Scale-Indicated_AirSpeed.png", cv::IMREAD_UNCHANGED);
+cv::Mat airspeedLadder = cv::imread("/Users/bernardolorenzini/Documents/MyGitRepositories/Textures/Airspeed-Limits-Ladder-Middle.png", cv::IMREAD_UNCHANGED);
 cv::Mat fpvExcessiveDeviation = cv::imread("/Users/bernardolorenzini/Documents/MyGitRepositories/Textures/FPV_Excessive_Deviation.png", cv::IMREAD_UNCHANGED);
+
+cv::Mat RollScaleTicks = cv::imread("/Users/bernardolorenzini/Documents/MyGitRepositories/Textures/Roll_Scale-Ticks.png", cv::IMREAD_UNCHANGED);
+
+cv::Mat Gears_locked_down = cv::imread("/Users/bernardolorenzini/Documents/MyGitRepositories/Textures/Gears_locked_down.png", cv::IMREAD_UNCHANGED);
+
+
 
 cv::Mat selectSymbology(int key);
 
