47863a62a5c55297dd325da12552ade3fadc178f
Bernardo Lorenzini <87033537+bernardolorenzini@users.noreply.github.com>
2023-08-16 14:29:26 -0300

    Add files via upload

---
+++ HUD/OpenCV_Course02/draw_display.hpp
@@ -0,0 +1,40 @@
+//
+//  draw_display.hpp
+//  OpenCV_Course02
+//
+//  Created by Bernardo Lorenzini on 13/08/23.
+//
+
+/*
+#ifndef DRAW_DISPLAY_HPP
+#define DRAW_DISPLAY_HPP
+
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <cmath>
+
+const float FULL_ROTATION = 360.0f;
+const float FULL_ROTATION_RADIANS = 2 * M_PI;
+
+float _restrict(float val, float upper_bound = 1.0f, float lower_bound = -1.0f);
+
+std::vector<cv::Point> _find_points(float m, float b, cv::Size frame_shape);
+
+void draw_roi(cv::Mat& frame, const std::map<std::string, float>& crop_and_scale_parameters);
+
+cv::Mat draw_hud(cv::Mat frame, float roll, float pitch, float fps,
+                bool is_good_horizon, bool recording = false);
+
+void draw_horizon(cv::Mat& frame, float roll, float pitch,
+                  float fov, cv::Scalar color, bool draw_groundline);
+
+void draw_surfaces(cv::Mat& frame, float left, float right, float top, float bottom,
+                   float ail_val, float elev_val, cv::Scalar surface_color);
+
+void draw_stick(cv::Mat& frame, float left, float top, float width,
+                float val1, float val2, float trim1, float trim2, cv::Scalar color);
+
+#endif // DRAW_DISPLAY_HPP
+
+
+*/
