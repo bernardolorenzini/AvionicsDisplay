195c8e619d83c51b1dae4fa2e78241d7f3c19ceb
Bernardo Lorenzini <87033537+bernardolorenzini@users.noreply.github.com>
2023-09-11 10:12:34 -0300

    Add files via upload

---
+++ HUD/OpenCV_Course02/DetermineGroundSpeedState_Logic_Manager_SpeedPackage_GroundSpeed.hpp
@@ -0,0 +1,24 @@
+//
+//  DetermineGroundSpeedState_Logic_Manager_SpeedPackage_GroundSpeed.hpp
+//  
+//
+//  Created by Bernardo Lorenzini on 11/09/23.
+//
+
+#ifndef DetermineGroundSpeedState_Logic_Manager_SpeedPackage_GroundSpeed_hpp
+#define DetermineGroundSpeedState_Logic_Manager_SpeedPackage_GroundSpeed_hpp
+
+#include <stdio.h>
+#include "kcg_types.h"
+
+// to be implemented
+extern GroundSpeedStateEnum_DisplayTypes DetermineGroundSpeedState_Logic_Manager_SpeedPackage_GroundSpeed(
+  /** ground speed validity (bool */
+kcg_bool bGroundSpeedValid,
+  /** Ground Speed (knots ) **/
+                                                                                                          
+kcg_real rGroundSpeed);
+
+
+
+#endif /* DetermineGroundSpeedState_Logic_Manager_SpeedPackage_GroundSpeed_hpp */
